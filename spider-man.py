1303ff1104c078e51145f1ca35e69c65673a4936
ajmalpoovanath <ajmal.poovanath@gmail.com>
2022-07-02 19:58:56 +0530

    drawing speed added

---
+++ spider-man.py
@@ -0,0 +1,23 @@
+from cgi import print_directory
+from turtle import *
+
+#drawing speed control
+speed(13)
+bgcolor("#990000")
+pensize(10)
+penup()
+goto(0,50)
+pendown()
+circle(-120)
+penup()
+circle(-120,-60)
+pendown()
+pensize(5)
+right(50)
+circle(70,55)
+right(85)
+circle(75,58)
+right(90)
+circle(70,55)
+right(90)
+circle(70,58)
